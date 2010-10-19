b5cc78f7c8d5b2a098ca0aed00e49086eca54a9a
Dmitry S. Kravtsov <idkravitz@gmail.com>
2010-10-19 22:42:49 +1100

    Initial commit, two modules (GUI and console interface)

---
+++ src/grant-shell.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Grant diagram manipulation program
+task is located at http://iexperts.ru/files/task2010.rar
+
+This is a command-line interface
+"""
+
+import sys
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ src/grant.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Grant diagram manipulation program
+task is located at http://iexperts.ru/files/task2010.rar
+
+This is an entry point for GUI interface on PyQt
+"""
+
+import sys
+from PyQt4 import QtGui
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+
+    widget = QtGui.QWidget()
+    widget.resize(250, 150)
+    widget.setWindowTitle('simple')
+    widget.show()
+
+    return app.exec_()
+
+if __name__ == '__main__':
+    sys.exit(main())
