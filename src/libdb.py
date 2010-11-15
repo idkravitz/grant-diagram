bcdbbf2f07dd2d6a35c76dacc62a10a01ac1031a
Dmitry S. Kravtsov <idkravitz@gmail.com>
2010-11-15 18:47:35 +1000

    Add strict constraints

--- src/libdb.py
+++ src/libdb.py
@@ -17,55 +17,100 @@ logger.addHandler(ch)
 def connect_database(db_name):
     logger.debug('-- connecting {0} database'.format(db_name))
     connection = sqlite3.connect(db_name) if os.path.isfile(db_name) else init_database(db_name)
+    return connection
 
-pk = lambda t: "{0} primary key".format(t)
-fk = lambda t, table, column="id": "{0} references {1} ({2}) on delete cascade on update cascade".format(t, table, column)
+def pk(t):
+    return "{0} primary key".format(t)
+
+def fk(t, table, column="id"):
+    return "{0} references {1} ({2}) on delete cascade on update cascade".format(t, table, column)
+
+def nn(t):
+    return "{0} not null".format(t)
+
+def unique(t):
+    return "{0} unique".format(t)
+
+
+def constraint(name="", body=None):
+    left_side = ""
+    if name:
+        left_side = "constraint " + name
+    return (left_side, body)
+
+def check_bool(field):
+    return constraint(
+        name=("bool_" + field),
+        body="check ({0} in (0,1))".format(field))
+
+def date_precendance(before, after):
+    return constraint(
+        name="date_precendace_{0}_and_{1}".format(before, after),
+        body="check ({0} < {1})".format(before, after))
+
+def check_date(field):
+    return constraint(
+        name="date_"+field,
+        body="check ({0} > 0)".format(field))
+
+def constraint_pk(*fields):
+    return constraint(body="primary key ({0})".format(",".join(fields)))
+
+def check_enum(field, *values):
+    return constraint(name="enum_"+field,
+        body="check ({0} in ({1}))".format(field, ",".join('"{0}"'.format(v) for v in values)))
 
 tables = OrderedDict([
     ("companies", OrderedDict(
         [
             ("id", pk("integer")),
-            ("name", "text")
+            ("name", unique(nn("text")))
         ])
     ),
     ("projects", OrderedDict(
         [
             ("id", pk("integer")),
-            ("begin_date", "integer not null"),
-            ("end_date", "integer not null")
+            ("begin_date", nn("integer")),
+            ("end_date", nn("integer")),
+            date_precendance("begin_date", "end_date"),
+            check_date("begin_date")
         ])
     ),
     ("developers", OrderedDict(
         [
-            ("full_name", "text"),
+            ("full_name", nn("text")),
             ("username", pk("text")),
-            ("company_id", fk("integer", "companies")),
-            ("password", "text"),
-            ("is_admin", "integer")
+            ("company_id", nn(fk("integer", "companies"))),
+            ("password", nn("text")),
+            ("is_admin", nn("integer")),
+            check_bool("is_admin")
         ])
     ),
     ("developers_distribution", OrderedDict(
         [
-            ("developer_username", fk("text", "developers", "username")),
-            ("project_id", fk("integer", "projects")),
-            ("is_manager", "integer"),
-            ("constraint developers_distribution_pk", "primary key(developer_username, project_id)")
+            ("developer_username", nn(fk("text", "developers", "username"))),
+            ("project_id", nn(fk("integer", "projects"))),
+            ("is_manager", nn("integer")),
+            constraint_pk("developer_username", "project_id"),
+            check_bool("is_manager")
         ])
     ),
     ("tasks", OrderedDict(
         [
             ("id", pk("integer")),
-            ("title", "text"),
-            ("description", "text"),
-            ("project_id", fk("integer", "projects")),
-            ("hours", "integer"),
-            ("status", "integer")
+            ("title", nn("text")),
+            ("description", nn("text")),
+            ("project_id", nn(fk("integer", "projects"))),
+            ("hours", nn("integer")),
+            ("status", nn("integer")),
+            check_enum("status", "active", "finished", "delayed")
         ])
     ),
     ("tasks_dependencies", OrderedDict(
         [
             ("task_id", fk("integer", "tasks")),
-            ("depended_task_id", fk("integer", "tasks"))
+            ("depended_task_id", fk("integer", "tasks")),
+            constraint_pk("task_id", "depended_task_id")
         ])
     ),
     ("contracts", OrderedDict(
@@ -74,17 +119,21 @@ tables = OrderedDict([
             ("company_id", fk("integer", "companies")),
             ("project_id", fk("integer", "projects")),
             ("date_of_creation", "integer"),
-            ("status", "integer")
+            ("status", "integer"),
+            check_date("date_of_creation"),
+            check_enum("status", "active", "finished", "delayed")
         ])
     ),
     ("reports", OrderedDict(
         [
             ("id", pk("integer")),
             ("developer_id", fk("integer", "developers", "username")),
-            ("task_id", fk("integer", "tasks")),
-            ("begin_date", "integer"),
-            ("end_date", "integer"),
-            ("description", "text")
+            ("task_id", nn(fk("integer", "tasks"))),
+            ("begin_date", nn("integer")),
+            ("end_date", nn("integer")),
+            ("description", nn("text")),
+            date_precendance("begin_date", "end_date"),
+            check_date("begin_date")
         ])
     ),
 ])
@@ -95,7 +144,7 @@ def init_database(db_name):
     for name, fields in tables.items():
         query = "create table {0} (\n{1}\n);".format(
             name,
-            ",\n".join(" " * 4 + " ".join(field) for field in fields.items()))
+            ",\n".join(" " * 4 + " ".join(col for col in field if col) for field in fields.items()))
         logger.debug(query)
         connection.execute(query)
     return connection
