979a9a602291061a01e88ace932231d16598b5b7
Dmitry S. Kravtsov <idkravitz@gmail.com>
2010-11-19 21:24:50 +1000

    Add new cmd-line option

--- src/grant-shell.py
+++ src/grant-shell.py
@@ -12,9 +12,16 @@ import sys
 import libdb
 import optparse
 
-def main():
+def parse_options():
     parser = optparse.OptionParser(usage='grant-shell.py [options]')
-    grant = libdb.Grant(echo=True)
+    parser.add_option("-d", "--database", dest="dbname", help="path to database", metavar="FILE")
+    return parser.parse_args()
+
+def main():
+    (options, args) = parse_options()
+    grant_args = {}
+    if options.dbname: grant_args['dbname'] = options.dbname
+    grant = libdb.Grant(echo=True, **grant_args)
     return 0
 
 if __name__ == '__main__':
